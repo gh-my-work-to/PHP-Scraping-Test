e468b5611401cd4c0423f9e82cc0f9d3fde6d75a
gh-my-work-to <my.work.tokorozawa@gmail.com>
2014-12-13 21:00:10 +0900

    #2

--- scraping_test.php
+++ scraping_test.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * http://news.google.co.jp/news?pz=1&cf=all&ned=jp&hl=ja&q
+ * からニュースのタイトル一覧を取得して表示する。
+ */
 include_once ('./simple_html_dom.php');
 
 ini_set('user_agent', 'My-Application/2.5');
@@ -7,27 +11,43 @@ $ret = getData();
 showData($ret);
 exit();
 //
+/**
+ * データを表示する
+ *
+ * @param 表示するデータ $ret        	
+ */
 function showData($ret)
 {
-	foreach ( $ret as $v )
+	header('contentType:text/plain;charset=utf-8');
+	
+	foreach($ret as $v)
 	{
-		echo $v ['title'] . "<br>\n";
+		echo "●" . $v['title'] . "\n";
 	}
 }
-//
+
+/**
+ * データを取得する
+ *
+ * @return multitype:
+ */
 function getData()
 {
-	$ret = array ();
+	$ret = array();
 	
 	// create HTML DOM
-	$html = file_get_html('http://localhost/sample.html');
-	// get news block
-	$ary = $html->find('ul.newslistTextTop', 0)->children;
-	foreach ( $ary as $ent )
+	$html = file_get_html('http://news.google.co.jp/news?pz=1&cf=all&ned=jp&hl=ja&q');
+	
+	// span.titletextを取得する
+	$ary = $html->find('span.titletext');
+	
+	foreach($ary as $ent)
 	{
-		$item ['title'] = $ent->find('a', 0)->plaintext;
+		// データを格納する
+		$item['title'] = trim($ent->plaintext);
 		array_push($ret, $item);
 	}
+	
 	// clean up memory
 	$html->clear();
 	unset($html);
